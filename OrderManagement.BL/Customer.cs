c545eb5328b83a63d3df13265c43f625528b338c
Matys <mateusz.sutor@icloud.com>
2020-09-02 22:36:05 +0200

    Utworzenie klasy Customer

---
+++ OrderManagement.BL/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.BL
+{
+    public class Customer
+    {
+        public int IdCustomer { get; private set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string EmailAddress { get; set; }
+
+        public string HomeAddress { get; set; }
+
+        public string WorkAddress { get; set; }
+        public string FirstNameAndLastName
+        {
+            get { return LastName + " " + FirstName; }
+        }
+   }
+}
